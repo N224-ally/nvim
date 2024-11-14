83a7a6180f9cddf6ce0637b6e137784c046486cf
niwe <bakakis101@gmail.com>
2024-11-14 13:46:25 -0600

    Added lua file from branch abt to be deleted

---
+++ init.lua
@@ -0,0 +1,8 @@
+-------------------------------------------------------------------
+-- General Configuration
+-------------------------------------------------------------------
+-- Basic Settings 
+vim.opt.number = true
+vim.opt.tabstop = 3
+vim.opt.relativenumber = true
+
